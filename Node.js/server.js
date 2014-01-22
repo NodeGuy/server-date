392c743cf1d9e1e3a583cee51c2fcc76f362e20e
David Braun <BallBearing@example.com>
2014-01-22 12:38:34 -0300

    Clean up Node.js example.

---
+++ Node.js/server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var
+	fs = require("fs"),
+	express = require('express'),
+	app = express(),
+	server;
+
+app.get("/ServerDate.js", function(req, res) {
+	fs.readFile('../lib/ServerDate.js', 'utf8', function (err, data) {
+		var now = Date.now();
+
+		if (err)
+			res.status(500);
+		else {
+			if (req.query.time) {
+				res.set("Content-Type", "application/json");
+				res.json(now);
+			}
+			else {
+				res.set("Content-Type", "text/javascript");
+				res.send(data + "(" + now + ");\n");
+			}
+		}
+	});
+});
+
+app.get("/", function(req, res) {
+	res.sendfile("index.html");
+});
+
+server = app.listen();
+console.log("Open http://localhost:" + server.address().port + " in a browser.");
