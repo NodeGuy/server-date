4fd47ac0009e3ad124907e332d939919d24c7767
David Braun <NodeGuy@users.noreply.github.com>
2023-07-03 00:34:00 -0400

    Export `fetchSampleImplementation`.

--- serverDate.js
+++ serverDate.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-const fetchSampleImplementation = async () => {
+export const fetchSampleImplementation = async () => {
   const requestDate = new Date();
 
   const { headers, ok, statusText } = await fetch(window.location, {
